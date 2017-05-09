66a97573e35cd4032e4231cd3ea11e252592a92a
tomhuhges <merzbear@gmail.com>
2017-05-09 16:29:13 +0100

    init

--- test/setup.js
+++ test/setup.js
@@ -1,15 +1,3 @@
 import jsdom from 'jsdom';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
-
-// get CodeMirror to render in tests
-global.getComputedStyle = window.getComputedStyle;
-const createRangeObj = {
-  setEnd: () => {},
-  setStart: () => {},
-  getBoundingClientRect: () => {},
-  getClientRects: () => [],
-};
-global.window.document.createRange = () => createRangeObj;
